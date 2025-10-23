0ae0c34217ed48ca3f6285e7b686ff6e8a824062
Sourav HM <souravhm5289@gmail.com>
2025-10-23 19:10:59 +0530

    Refactor: Remove data preparation code
    Removed unnecessary code for data preparation and scaling.

--- Stock-Price-Prediction.py
+++ Stock-Price-Prediction.py
@@ -22,31 +22,3 @@ plt.xlabel('Date',fontsize=18)
 plt.ylabel('Close Price USD $',fontsize=18)
 plt.show()
 
-#Create a new dataframe with only the 'close column'
-data=df.filter(['Close'])
-
-#convert the dataframe to numpy array
-dataset=data.values
-
-#Get the number of rows to train the model on
-training_data_len=math.ceil(len(dataset) * .8)
-training_data_len
-
-#scale the data
-scaler=MinMaxScaler(feature_range=(0,1))
-scaled_data=scaler.fit_transform(dataset)
-scaled_data
-
-#training dataset
-#create the scaled training data set
-train_data=scaled_data[0:training_data_len , :]
-#split the data into x_train and y_train data sets
-x_train=[]
-y_train=[]
-for i in range(60,len(train_data)):
-x_train.append(train_data[i-60:i,0])
-y_train.append(train_data[i,0])
-if i<=61:
-print(x_train)
-print(y_train)
-print()
