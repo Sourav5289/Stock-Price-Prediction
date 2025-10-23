bb41a24e7f059100eddb78bac4007952811469aa
Sourav HM <souravhm5289@gmail.com>
2025-10-23 19:31:29 +0530

    Stock Price Prediction

--- Stock-Price-Prediction.py
+++ Stock-Price-Prediction.py
@@ -22,3 +22,23 @@ plt.xlabel('Date',fontsize=18)
 plt.ylabel('Close Price USD $',fontsize=18)
 plt.show()
 
+#Create a new dataframe with only 'close column'
+data = df.filter(['Close'])
+
+#Convert the dataframe to numpy array
+dataset = data.values
+
+#Get the numbr of rows to train the model
+training_data_len = math.ceil(len(dataset) * .8)
+
+#Scale the data
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(dataset)
+scaled_data
+
+#Training dataset
+#Create the scaled training dataset
+train_data = scaled_data[0 : training_data_len  ,: ]
+#Split the dataset into two parts
+x_train = []
+y_tarin = []
