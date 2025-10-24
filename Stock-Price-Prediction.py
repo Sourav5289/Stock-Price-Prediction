ff85ca69c9fa3bf45f89cdb5cfe6553279545f55
Sourav HM <souravhm5289@gmail.com>
2025-10-24 08:35:18 +0530

    Update Stock-Price-Prediction.py

--- Stock-Price-Prediction.py
+++ Stock-Price-Prediction.py
@@ -42,3 +42,11 @@ train_data = scaled_data[0 : training_data_len  ,: ]
 #Split the dataset into two parts
 x_train = []
 y_tarin = []
+
+for i in range(60,len(train_data)):
+    x_train.append(train_data[i-60:i,0])
+    y_train.append(train_data[i,0])
+       if i<=61:
+           print(x_train)
+           print(y_train)
+           print()
