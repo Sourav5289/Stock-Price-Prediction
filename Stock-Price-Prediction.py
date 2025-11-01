6477437a570594402ef947f5eee1d0bf082f8552
Sourav HM <souravhm5289@gmail.com>
2025-11-01 10:31:35 +0530

    Update Stock-Price-Prediction.py

--- Stock-Price-Prediction.py
+++ Stock-Price-Prediction.py
@@ -56,12 +56,15 @@ print(x_train)
 print(y_train)
 print()
 
+
 #convert the x_train and y_train to numpy arrays
 x_train , y_train = np.array(x_train), np.array(y_train)
+
 
 #reshape the data
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 x_train.shape
+
 
 #Build the LSTM model
 model=Sequential()
@@ -70,18 +73,41 @@ model.add(LSTM(50,return_sequences=False))
 model.add(Dense(25))
 model.add(Dense(1))
 
+
 #compile the model
 model.compile(optimizer='adam' , loss='mean_squared_error')
+
 
 #train the model
 model.fit(x_train,y_train , batch_size=1, epochs=1)
 
+
 # Create the testing data set
 # Create a new array containing scaled values from index training_data_len-60 to the end
 test_data = scaled_data[training_data_len - 60:, :]
+
 
 # Create the data sets x_test and y_test
 x_test = []
 y_test = dataset[training_data_len:, :]
 for i in range(60, len(test_data)):
 x_test.append(test_data[i-60:i, 0])
+
+
+# Convert the data to a numpy array
+x_test = np.array(x_test)
+
+
+# The data is already in the correct 3D shape (samples, time steps, features)
+# x_test.shape should be (number of test samples, 60, 1)
+x_test = np.reshape(x_test, (x_test.shape[0] , x_test.shape[1],1))
+
+
+#Get the models predicted price values
+predictions = model.predict(x_test)
+predictions = scaler.inverse_transform(predictions)
+
+
+#get the root mean square error(RMSE)
+rmse = np.sqrt(np.mean(predictions - y_test)**2)
+rmse
