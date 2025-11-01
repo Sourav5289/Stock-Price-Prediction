485c74de354efeb1346e0ca9dafa20dbd2a24211
Sourav HM <souravhm5289@gmail.com>
2025-11-01 10:29:43 +0530

    Update Stock-Price-Prediction.py

--- Stock-Price-Prediction.py
+++ Stock-Price-Prediction.py
@@ -58,6 +58,30 @@ print()
 
 #convert the x_train and y_train to numpy arrays
 x_train , y_train = np.array(x_train), np.array(y_train)
+
 #reshape the data
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 x_train.shape
+
+#Build the LSTM model
+model=Sequential()
+model.add(LSTM(50,return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(LSTM(50,return_sequences=False))
+model.add(Dense(25))
+model.add(Dense(1))
+
+#compile the model
+model.compile(optimizer='adam' , loss='mean_squared_error')
+
+#train the model
+model.fit(x_train,y_train , batch_size=1, epochs=1)
+
+# Create the testing data set
+# Create a new array containing scaled values from index training_data_len-60 to the end
+test_data = scaled_data[training_data_len - 60:, :]
+
+# Create the data sets x_test and y_test
+x_test = []
+y_test = dataset[training_data_len:, :]
+for i in range(60, len(test_data)):
+x_test.append(test_data[i-60:i, 0])
