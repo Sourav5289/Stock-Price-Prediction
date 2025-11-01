b964b842bfa47504515a52e4a10a5ceb242e1588
Sourav HM <souravhm5289@gmail.com>
2025-11-01 10:26:02 +0530

    Update Stock-Price-Prediction.py

--- Stock-Price-Prediction.py
+++ Stock-Price-Prediction.py
@@ -12,3 +12,15 @@ plt.style.use ('fivethirtyeight')
 
 #get the stock quote
 df = yf.download('AAPL', start='2012-01-01', end='2019-12-17')
+
+#get the number of rows and columns in data set
+df.shape
+
+
+#visualize the closing price histroy
+plt.figure(figsize=(16,8))
+plt.title('Close Price History')
+plt.plot(df['Close'])
+plt.xlabel('Date',fontsize=18)
+plt.ylabel('Close Price USD ($)',fontsize=18)
+plt.show()
