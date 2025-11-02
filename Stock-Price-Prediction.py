4fc4d103696344208ff2ba17f8a55bfe34d3b8d2
Sourav HM <souravhm5289@gmail.com>
2025-11-02 08:33:51 +0530

    Update Stock-Price-Prediction.py

--- Stock-Price-Prediction.py
+++ Stock-Price-Prediction.py
@@ -9,13 +9,11 @@ from keras.layers import Dense, LSTM
 import matplotlib.pyplot as plt
 plt.style.use ('fivethirtyeight')
 
-
 #get the stock quote
 df = yf.download('AAPL', start='2012-01-01', end='2019-12-17')
 
 #get the number of rows and columns in data set
 df.shape
-
 
 #visualize the closing price histroy
 plt.figure(figsize=(16,8))
@@ -35,12 +33,10 @@ dataset=data.values
 training_data_len=math.ceil(len(dataset) * .8)
 training_data_len
 
-
 #scale the data
 scaler=MinMaxScaler(feature_range=(0,1))
 scaled_data=scaler.fit_transform(dataset)
 scaled_data
-
 
 #training dataset
 #create the scaled training data set
@@ -56,15 +52,12 @@ print(x_train)
 print(y_train)
 print()
 
-
 #convert the x_train and y_train to numpy arrays
 x_train , y_train = np.array(x_train), np.array(y_train)
-
 
 #reshape the data
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 x_train.shape
-
 
 #Build the LSTM model
 model=Sequential()
@@ -73,19 +66,15 @@ model.add(LSTM(50,return_sequences=False))
 model.add(Dense(25))
 model.add(Dense(1))
 
-
 #compile the model
 model.compile(optimizer='adam' , loss='mean_squared_error')
-
 
 #train the model
 model.fit(x_train,y_train , batch_size=1, epochs=1)
 
-
 # Create the testing data set
 # Create a new array containing scaled values from index training_data_len-60 to the end
 test_data = scaled_data[training_data_len - 60:, :]
-
 
 # Create the data sets x_test and y_test
 x_test = []
@@ -93,20 +82,16 @@ y_test = dataset[training_data_len:, :]
 for i in range(60, len(test_data)):
 x_test.append(test_data[i-60:i, 0])
 
-
 # Convert the data to a numpy array
 x_test = np.array(x_test)
-
 
 # The data is already in the correct 3D shape (samples, time steps, features)
 # x_test.shape should be (number of test samples, 60, 1)
 x_test = np.reshape(x_test, (x_test.shape[0] , x_test.shape[1],1))
 
-
 #Get the models predicted price values
 predictions = model.predict(x_test)
 predictions = scaler.inverse_transform(predictions)
-
 
 #get the root mean square error(RMSE)
 rmse = np.sqrt(np.mean(predictions - y_test)**2)
@@ -127,18 +112,14 @@ plt.plot(valid[['Close', 'Predictions']])
 plt.legend(['Train', 'Val', 'Predictions'], loc='lower right')
 plt.show()
 
-
 #show the valid and predicted prices
 valid
-
 
 #Get the quote
 apple_quote = yf.download('AAPL', start='2012-01-01', end='2019-12-17')
 
-
 # Print columns to diagnose
 print(apple_quote.columns)
-
 
 #create new dataframe with only the 'Close' column
 new_df = apple_quote['Close']
